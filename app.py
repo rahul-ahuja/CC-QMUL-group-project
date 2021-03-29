09c9226cfa393c47d18ad3cf644ec8747bf043be
rahul-ahuja <rahul.ahuja@live.com>
2021-03-29 21:54:58 +0100

    Update and rename stocks.py to app.py

--- app.py
+++ app.py
@@ -3,14 +3,19 @@ import pprint
 import os
 import urllib.parse
 from flask import Flask, redirect, session, jsonify, request
-import sqlite3
 from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
+import psycopg2
+
 
 
 app = Flask(__name__)
 
-conn = sqlite3.connect('stocks.db', check_same_thread=False)
+conn = psycopg2.connect('host=127.0.0.1 dbname=stocks user=developer4 password=dev_pswd')
 cur = conn.cursor()
+conn.set_session(autocommit=True)
+
+
 
 sess = {}
 api_key = "pk_f99acd61740946c9b13696784a19452e"
@@ -37,18 +42,14 @@ def get_records(symbol):
 @app.route('/register', methods = ['POST'])
 def register():
 	new_user = {'name': request.json['name'], 'pswd': generate_password_hash(request.json['pswd'])}
-	#db.update({new_user['name']: new_user['pswd']})
-	cur.execute('''INSERT INTO users (username, hash) VALUES (?, ?)''', (new_user['name'], new_user['pswd']))
-	conn.commit()
+	cur.execute('''INSERT INTO stocks.users (username, hash) VALUES (%s, %s)''', (new_user['name'], new_user['pswd']))
 	return jsonify({'message': 'Account registered: {}'.format(request.json['name'])}), 200
 	
 
 @app.route("/login", methods=["POST"])
 def login():
-	#session.clear()
-	user = {'name': request.json['name'], 'pswd': request.json['pswd']}
-	
-	cur.execute('''SELECT * FROM users WHERE username = ?''', (user['name'], ))
+	user = {'name': request.json['name'], 'pswd': request.json['pswd']}	
+	cur.execute('''SELECT * FROM stocks.users WHERE username = (%s)''', (user['name'], ))
 	#conn.commit()
 	row = cur.fetchall()
 	#print(row[0][2])
@@ -64,10 +65,10 @@ def login():
 @app.route("/reset_password", methods=["PUT"])
 def reset_password():
 	user = {'name': request.json['name'], 'pswd': request.json['pswd'], 'new_pswd': generate_password_hash(request.json['new_pswd'])}
-	cur.execute('''SELECT * FROM users WHERE username = ?''', (user['name'], ))
+	cur.execute('''SELECT * FROM stocks.users WHERE username = (%s)''', (user['name'], ))
 	row = cur.fetchall()
 	if check_password_hash(row[0][2], user['pswd']):
-		cur.execute('''UPDATE users SET hash =?''', (user['new_pswd'], ))
+		cur.execute('''UPDATE stocks.users SET hash =(%s)''', (user['new_pswd'], ))
 		return jsonify("Password changed! ".format(user['name']))
 	else:
 		return jsonify("Incorrect credentials!"), 401
@@ -78,12 +79,10 @@ def buy(symbol):
 	response = requests.get(url)
 	response.raise_for_status()
 	quote = response.json()
+	time1 = datetime.now()
 	try:
-		cur.execute('''INSERT INTO sales (symbol, price, user_name, share_name) VALUES (?, ?, ?, ?)''', 
+		cur.execute('''INSERT INTO stocks.sales (symbol, price, user_name, share_name) VALUES (%s, %s, %s, %s)''', 
 	    	(quote["symbol"] , float(quote["latestPrice"]), sess['name'], quote["companyName"]))
-		conn.commit()
-		#row = cur.execute('''SELECT * FROM sales''')
-		#print(row.fetchall()[0])
 		return jsonify("Bought! {} at {} for {}".format(quote["companyName"], float(quote["latestPrice"]), sess['name'])), 201
 	except KeyError:
 		return jsonify("Kindly register or login first!"), 404
@@ -91,24 +90,16 @@ def buy(symbol):
 @app.route("/records", methods=["GET"])
 def display_records():
 	try:
-		row = cur.execute('''SELECT * FROM sales WHERE user_name = ?''', (sess['name'], ))
-		return jsonify(row.fetchall()), 200
+		cur.execute('''SELECT * FROM stocks.sales WHERE user_name = (%s)''', (sess['name'], ))
+		return jsonify(cur.fetchall()), 200
 	except KeyError:
 		return jsonify("Kindly register or login first!"), 404
 
-	#return jsonify(row.fetchall()), 200
-
 @app.route("/return/<r_id>", methods=['DELETE'])
 def return_stocks(r_id):
-	cur.execute('''DELETE FROM sales WHERE user_name = ? AND id=?''', (sess['name'], r_id))
-	conn.commit()
+	cur.execute("""DELETE FROM stocks.sales WHERE id = {}""".format(r_id))	
 	return jsonify("Success!"), 200
 
-#stocks = {
-#"name": quote["companyName"],
-#"price": float(quote["latestPrice"]),
-#"symbol": quote["symbol"]
-#}
 
 if __name__ == '__main__':
 	app.run(debug=True)
