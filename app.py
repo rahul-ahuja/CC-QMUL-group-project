a1cdbfc9d28c6255968b6006a0deb9d3a93c6faf
rahul-ahuja <rahul.ahuja@live.com>
2021-04-02 18:41:50 +0100

    Update app.py
    comments added

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#importing libraries
 import requests
 import pprint
 import os
@@ -8,17 +9,20 @@ from datetime import datetime
 import psycopg2
 
 
-
+#initializing the app as Flask class
 app = Flask(__name__)
+
+#making connection to the postgres database with the role of the developer
 
 conn = psycopg2.connect('host=127.0.0.1 dbname=stocks user=developer4 password=dev_pswd')
 cur = conn.cursor()
 conn.set_session(autocommit=True)
 
 
-
+#sess dictionary will carry the temporal information about the user across the functions
 sess = {}
-api_key = "pk_f99acd61740946c9b13696784a19452e"
+api_key = "pk_f99acd61740946c9b13696784a19452e" #API Key to IEX API
+# Here is the page to the documentation to IEX API https://iexcloud.io/docs/api/
 
 
 @app.route('/')
@@ -28,6 +32,10 @@ def hello():
 
 @app.route('/stocks/<symbol>', methods=['GET']) 
 def get_records(symbol): 
+	'''
+	symbol: Pass the symbol of the stock as an endpoint
+	return: displays the stock's name, price and symbol 
+	'''
 	url = f"https://cloud-sse.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}"
 	response = requests.get(url)
 	response.raise_for_status()
@@ -41,6 +49,9 @@ def get_records(symbol):
 
 @app.route('/register', methods = ['POST'])
 def register():
+	'''
+	return: the function is for the user to register for the account
+	'''
 	new_user = {'name': request.json['name'], 'pswd': generate_password_hash(request.json['pswd'])}
 	cur.execute('''INSERT INTO stocks.users (username, hash) VALUES (%s, %s)''', (new_user['name'], new_user['pswd']))
 	return jsonify({'message': 'Account registered: {}'.format(request.json['name'])}), 200
@@ -48,6 +59,9 @@ def register():
 
 @app.route("/login", methods=["POST"])
 def login():
+	'''
+	return: this function is for the user to login 
+	'''
 	user = {'name': request.json['name'], 'pswd': request.json['pswd']}	
 	cur.execute('''SELECT * FROM stocks.users WHERE username = (%s)''', (user['name'], ))
 	#conn.commit()
@@ -64,6 +78,9 @@ def login():
 
 @app.route("/reset_password", methods=["PUT"])
 def reset_password():
+	'''
+	this function is to user reset password
+	'''
 	user = {'name': request.json['name'], 'pswd': request.json['pswd'], 'new_pswd': generate_password_hash(request.json['new_pswd'])}
 	cur.execute('''SELECT * FROM stocks.users WHERE username = (%s)''', (user['name'], ))
 	row = cur.fetchall()
@@ -75,6 +92,11 @@ def reset_password():
 
 @app.route("/buy/<symbol>", methods=["POST"])
 def buy(symbol):
+	'''
+	this function is to buy the stocks
+	symbol: which stock with symbol to buy
+	return: new stock details will be added to their page after making the post request
+	'''
 	url = f"https://cloud-sse.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}"
 	response = requests.get(url)
 	response.raise_for_status()
@@ -89,6 +111,9 @@ def buy(symbol):
 
 @app.route("/records", methods=["GET"])
 def display_records():
+	'''
+	this function displays the records of stocks of the logged in user
+	'''
 	try:
 		cur.execute('''SELECT * FROM stocks.sales WHERE user_name = (%s)''', (sess['name'], ))
 		return jsonify(cur.fetchall()), 200
@@ -97,6 +122,10 @@ def display_records():
 
 @app.route("/return/<r_id>", methods=['DELETE'])
 def return_stocks(r_id):
+	'''
+	r_id: ID of the stock that user wants to return
+	return: after making this delete request, the stock will be deleted from the display page of the user
+	'''
 	cur.execute("""DELETE FROM stocks.sales WHERE id = {}""".format(r_id))	
 	return jsonify("Success!"), 200
 
