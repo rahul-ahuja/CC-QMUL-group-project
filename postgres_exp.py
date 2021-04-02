692dcf2af861216d83d8cb8297be40975b187f61
rahul-ahuja <rahul.ahuja@live.com>
2021-04-02 18:40:27 +0100

    Update postgres_exp.py
    comments added

--- postgres_exp.py
+++ postgres_exp.py
@@ -1,13 +1,12 @@
 import psycopg2
 
+#making connection to the stocks database
 conn = psycopg2.connect('host=127.0.0.1 dbname=stocks user=developer4 password=dev_pswd')
-
 cur = conn.cursor()
-
 conn.set_session(autocommit=True)
 
-
+#developer does not have privilege to drop any table from the schema
 cur.execute('''DROP TABLE IF EXISTS stocks.users''')
 
-cur.execute('''SELECT * FROM stocks.sales''')
+cur.execute('''SELECT * FROM stocks.users''')
 print(cur.fetchall())
