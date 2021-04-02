059bb9531d406173ba3c1c2cc244a664670c09f9
rahul-ahuja <rahul.ahuja@live.com>
2021-04-02 18:41:03 +0100

    Update postgres_setup.py
    comments added

--- postgres_setup.py
+++ postgres_setup.py
@@ -1,20 +1,21 @@
 import psycopg2
 
+#making connection to the postgres database called stocks as an owner- Database Admin
 conn = psycopg2.connect('host=127.0.0.1 dbname=stocks user=dba password=dba_pswd')
-
 cur = conn.cursor()
-
 conn.set_session(autocommit=True)
 
+#creating the schema of the database called stocks
 try:
 	cur.execute('''CREATE SCHEMA stocks''')
 except:
 	print('Schema already created')
-#create users table
 
+#making user the table with same does not exists
 cur.execute('''DROP TABLE IF EXISTS stocks.users''')
 cur.execute('''DROP TABLE IF EXISTS stocks.sales''')
 
+#creating users tables in the schema
 cur.execute('''CREATE TABLE stocks.users (id SERIAL PRIMARY KEY, 
 	username TEXT NOT NULL, 
 	hash TEXT NOT NULL)''')
@@ -28,23 +29,24 @@ cur.execute('''CREATE TABLE stocks.sales (id SERIAL PRIMARY KEY,
 
 
 #cur.execute('''REVOKE SELECT, INSERT ON TABLE users FROM readwrite3''')
-
+#creating a role for role-based policy
 try:
 	cur.execute('''CREATE ROLE readwrite4''')
 except:
 	print('Role already created')
 
+#granting the role with certain privileges
+
 cur.execute('''GRANT CONNECT ON DATABASE stocks TO readwrite4''')
-#cur.execute('''GRANT INSERT ON SCHEMA stocks TO readwrite4''')
 cur.execute('''GRANT SELECT, INSERT, UPDATE ON TABLE stocks.users TO readwrite4''')
 cur.execute('''GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE stocks.sales TO readwrite4''')
 cur.execute('''GRANT USAGE ON ALL SEQUENCES IN SCHEMA stocks TO readwrite4''')
 
-
+#assigning the role to the user called developer4 who will work on the main application
 try:
 	cur.execute("CREATE USER developer4 WITH PASSWORD 'dev_pswd'")
 except:
 	print('User already created')
 
 cur.execute('''GRANT readwrite4 TO developer4''')
-conn.close()
+conn.close() #closing the database connection
